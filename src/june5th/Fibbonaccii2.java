ee8b8ef19a47f356674f51e09df19461f71afa06
jahnavi0904 <jahnavikatta2000@gmail.com>
2023-06-06 15:56:09 -0500

    commit done

--- src/june5th/Fibbonaccii2.java
+++ src/june5th/Fibbonaccii2.java
@@ -1,9 +1,10 @@
 package june5th;
 
-public class Fibbonacci {
+public class Fibbonaccii2 {
     public static void main(String[] args){
         int a=0 ,b=1,n=10;
         System.out.println(a);
+        System.out.println(b);
         for(int i=2;i<=n;i++) {
             int temp=a;
             a=b;
