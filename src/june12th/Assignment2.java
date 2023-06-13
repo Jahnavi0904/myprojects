7b09975c1a445dc745541c1ce6c7697f4152f3f0
Jahnavi0904 <129913683+Jahnavi0904@users.noreply.github.com>
2023-06-13 15:17:41 -0500

    commit done

---
+++ src/june12th/Assignment2.java
@@ -0,0 +1,19 @@
+package june12th;
+class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
+
+public class Assignment2{
+    public static void main(String[] args) {
+        try {
+            int i=2/0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch(CustomException y){
+        CustomException x = new CustomException(y.getMessage());
+//            throw x;
+        }
+    }
+}
